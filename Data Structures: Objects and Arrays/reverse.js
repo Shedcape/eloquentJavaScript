d8a1a59bc9ee98fa339558cebd5a634eb912389f
Shedcape <59801532+Shedcape@users.noreply.github.com>
2022-06-23 18:33:15 +0200

    Delete Data Structures: Objects and Arrays directory

--- Data Structures: Objects and Arrays/reverse.js
+++
@@ -1,8 +0,0 @@
-function reverse(arr){
-    const result = [];
-    const arrCopy = arr.slice();
-    for (let i = arrCopy.length-1; i>=0; i--){
-        result.push(arrCopy.pop());
-    }
-    return result;
-}
